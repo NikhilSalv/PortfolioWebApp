0d18702d32078f5b9dc174052c55710c10ccbe7e
Nikhil the coder <NikhilSalvi@123>
2025-12-05 09:58:44 +0530

    Fix: server.js path error fixed 2

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", (req, res) => {
+app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+s
